71f8178c75e71224cbcb72192affa4b5175fd8eb
Chris Dunne <chrisdunne66@gmail.com>
2021-02-15 16:36:15 +0000

    Setting the popup to show as green on developers.chrome.com

--- background.js
+++ background.js
@@ -1,26 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-      "id": "sampleContextMenu",
-      "title": "Sample Context Menu",
-      "contexts": ["selection"]
-    });
-  });
-
-  chrome.runtime.onInstalled.addListener(function() {
-    // ERROR! Events must be registered synchronously from the start of
-    // the page.
     chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
-      });
-
+      console.log('The color is green.');
+    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
+      chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {hostEquals: 'developer.chrome.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+      }]);
     });
-    
   });
